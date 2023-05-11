dffdcab5c72ea6178667fb46dbabee62b334f8d7
Anastasia <nastena.maiorova@gmail.com>
2023-05-11 12:25:27 +0300

    Создан проект интернет-магазина

---
+++ urls.py
@@ -0,0 +1,31 @@
+"""shop_garaz URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import include
+from django.urls import path
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.debug import default_urlconf # восстановили ракету
+from django.conf.urls.static import static
+from shop_garaz import settings
+
+urlpatterns = [
+    path('', default_urlconf),
+    path('admin/', admin.site.urls),
+    path('shop/', include('shop.urls'))
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
